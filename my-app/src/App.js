259c831678a5cdcbeac911d8a8c669d02acbc280
Erbol94 <erbolshakeev@gmail.com>
2022-05-16 17:13:38 +0600

    Lesson 3

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './App.css';
+import News from "./pages/news/News";
+
+
+function App() {
+  return (
+    <div>
+        <h1>DZ-3</h1>
+        <News />
+    </div>
+  );
+}
+
+export default App;
